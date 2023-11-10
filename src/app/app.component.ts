aaf275beda2b2d23acfcff0e4ec3af97698798c4
moafk <mwafak_sh@hotmail.com>
2023-11-10 18:32:25 +0300

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    // "../../node_modules/bootstrap/dist/css/bootstrap.css",
+    'app.component.css' ]
+})
+export class AppComponent {
+
+}
